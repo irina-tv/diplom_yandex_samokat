a516d14181bd1e5b5fa4c62b6371ce9bf921f1f5
itverdikov <irina@tverdikov.com>
2024-10-06 13:27:49 -0400

    add tests

---
+++ configuration.py
@@ -0,0 +1,4 @@
+# Ирина Твердикова, 22-я когорта — Финальный проект. Инженер по тестированию плюс
+
+URL_SERVICE = "https://115eb43a-8c61-42e0-bcdf-afec37988bed.serverhub.praktikum-services.ru/"
+CREAT_ORDERS = "/api/v1/orders"
---
+++ data.py
@@ -0,0 +1,13 @@
+# Ирина Твердикова, 22-я когорта — Финальный проект. Инженер по тестированию плюс
+
+order_body = {
+        "firstName": "mira",
+        "lastName": "tver",
+        "address": "Konoha, 142 apt.",
+        "metroStation": 4,
+        "phone": "+7 800 355 35 35",
+        "rentTime": 5,
+        "deliveryDate": "2020-06-06",
+        "comment": "Saske, come back to Konoha",
+        "color": ["BLACK"]
+    }
