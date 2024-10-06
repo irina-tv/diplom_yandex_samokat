b81673acadadf932ac9da54f9a8765295444fad2
itverdikov <irina@tverdikov.com>
2024-10-06 17:58:11 -0400

    add tests

--- configuration.py
+++ configuration.py
@@ -1,4 +1,5 @@
 # Ирина Твердикова, 22-я когорта — Финальный проект. Инженер по тестированию плюс
 
-URL_SERVICE = "https://115eb43a-8c61-42e0-bcdf-afec37988bed.serverhub.praktikum-services.ru/"
+URL_SERVICE = "https://f75664ea-2024-4695-87df-e1e6ee80a2b7.serverhub.praktikum-services.ru/"
 CREAT_ORDERS = "/api/v1/orders"
+TRACK_URL = "/api/v1/orders/track?t="
--- test_order.py
+++ test_order.py
@@ -1,20 +1,4 @@
 # Ирина Твердикова, 22-я когорта — Финальный проект. Инженер по тестированию плюс
-
-import configuration
-import requests
-import data
-
-# Создание заказа
-def create_order(body):
-    return requests.post (configuration.URL_SERVICE + configuration.CREAT_ORDERS,
-                         json=body)
-
-# Получение заказа по номеру трекера
-def get_order(track_number):
-    get_order_url = f"{configuration.URL_SERVICE}/api/v1/orders/track?t={track_number}"
-    response = requests.get(get_order_url)
-    return response
-
 # Автотест
 def test_order_creation_and_retrieval():
     response = create_order(data.order_body)
